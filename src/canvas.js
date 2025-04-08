b6b79db5de26f8e7ed08932cf0c8a9f0018297bd
Ganeshn <ganesh.nikale@ibm.com>
2025-04-08 13:22:40 +0530

    adding position corrections

--- src/canvas.js
+++ src/canvas.js
@@ -7,8 +7,12 @@ const CANVAS_WIDTH = 10 * SCALE * 8;
 const CANVAS_HEIGHT = 10 * SCALE * 10;
 const OBJECT_SIZES = {
     "3x3": { width: 3 * SCALE, height: 3 * SCALE },
-    "2x2": { width: 2 * SCALE, height: 2 * SCALE },
-    "5x2": { width: 5 * SCALE, height: 2 * SCALE },
+    "4x3": { width: 4 * SCALE, height: 3 * SCALE },
+    "5x3": { width: 5 * SCALE, height: 3 * SCALE },
+    "6x3": { width: 6 * SCALE, height: 3 * SCALE },
+    "8x3": { width: 8 * SCALE, height: 3 * SCALE },
+
+
 };
 
 export default function CanvasApp() {
@@ -16,6 +20,7 @@ export default function CanvasApp() {
         { id: 1, x: 50, y: 50, width: OBJECT_SIZES["3x3"].width, height: OBJECT_SIZES["3x3"].height },
     ]);
     const [selectedSize, setSelectedSize] = useState("3x3");
+
 
     const addObject = () => {
         const newObject = {
@@ -55,18 +60,21 @@ export default function CanvasApp() {
             <div className="flex flex-col gap-2">
                 <select value={selectedSize} onChange={(e) => setSelectedSize(e.target.value)} className="border p-2">
                     <option value="3x3">3x3 feet</option>
-                    <option value="2x2">2x2 feet</option>
-                    <option value="5x2">5x2 feet</option>
+                    <option value="4x3">4x3 feet</option>
+                    <option value="5x3">5x3 feet</option>
+                    <option value="6x3">6x3 feet</option>
+                    <option value="8x3">8x3 feet</option>
+
                 </select>
                 <button onClick={addObject} className="bg-blue-500 text-white p-2 rounded">Add Object</button>
             </div>
 
             {/* Preview Area */}
             <Card className="p-4">
-                <p>Preview:</p>
+                <h3> Mobile preview Area</h3>
                 <div
                     className="border bg-gray-200"
-                    style={{ width: CANVAS_WIDTH, height: CANVAS_HEIGHT }}
+                    style={{ width: CANVAS_WIDTH, height: CANVAS_HEIGHT, position: 'relative' }}
                 >
                     {objects.map((obj) => (
                         <div
@@ -75,7 +83,9 @@ export default function CanvasApp() {
                             style={{
                                 width: obj.width,
                                 height: obj.height,
-                                transform: `translate(${obj.x}px, ${obj.y}px)`,
+                                position: 'absolute',
+                                top: `${obj.y}px`,
+                                left: `${obj.x}px`
                             }}
                         />
                     ))}
